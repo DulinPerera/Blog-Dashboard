43b628a1ae9d772e817a339c562729b480720d9f
DulinPerera <115730490+DulinPerera@users.noreply.github.com>
2024-05-02 23:24:11 +0530

    Complete signin page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -30,7 +30,7 @@ export default function SignUp() {
       }
       setLoading(false)
       if(res.ok){
-        navigate('/')
+        navigate('/signin')
       }
     }catch(error){
       setErrorMessage(error.message);
@@ -86,7 +86,7 @@ export default function SignUp() {
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to = "/sign-in" className="text-blue-500">
+            <Link to = "/signin" className="text-blue-500">
               Sign In
             </Link>
           </div> 
